d2a9646e6eaf868864df2c261a1b9d0fc7b8607f
Oscar <ew12136@gmail.com>
2025-08-09 11:26:30 +0800

    導入 Alembic

---
+++ alembic/versions/2025_08_09_1114-c6dd3264fae3_建立基準線_現有資料庫結構.py
@@ -0,0 +1,25 @@
+"""建立基準線：現有資料庫結構
+
+Revision ID: c6dd3264fae3
+Revises:
+Create Date: 2025-08-09 11:14:15.156596
+
+"""
+
+from typing import Sequence, Union
+
+# revision identifiers, used by Alembic.
+revision: str = 'c6dd3264fae3'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
